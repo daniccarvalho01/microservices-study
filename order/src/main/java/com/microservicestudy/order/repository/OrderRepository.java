5faf3da7e86cddc25599bd2872579c120c87da55
Daniela Carvalho <daniela.suppino@gmail.com>
2022-01-15 17:56:21 -0200

    Criando OrderRepository

---
+++ order/src/main/java/com/microservicestudy/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.microservicestudy.order.repository;
+
+import com.microservicestudy.order.domain.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository <Order, Long> {
+}
