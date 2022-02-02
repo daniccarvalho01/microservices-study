c1751990eece2d1be87dfa5820e7e53a41f2998f
Daniela Carvalho <daniela.suppino@gmail.com>
2022-02-02 14:45:04 -0200

    Correçoes Lombok

--- store/src/main/java/com/microservicesstudy/store/domain/request/StoreRequest.java
+++ store/src/main/java/com/microservicesstudy/store/domain/request/StoreRequest.java
@@ -4,19 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class StoreRequest {
 
     private String name;
-
-    public StoreRequest(String name, LocalDate dateCreate, LocalDate dateUpdate) {
-        this.name = name;
-
-    }
-
 }
