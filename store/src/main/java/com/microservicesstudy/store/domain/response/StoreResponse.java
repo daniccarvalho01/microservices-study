d25e9ca26c0bd877e797b856964c6f8757c72b56
daniccarvalho01 <daniela.suppino@gmail.com>
2022-03-07 19:59:51 -0300

    Corrigindo detalhes da Store

--- store/src/main/java/com/microservicesstudy/store/domain/response/StoreResponse.java
+++ store/src/main/java/com/microservicesstudy/store/domain/response/StoreResponse.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class StoreResponse {
 
     private Long id;
--- store/src/main/java/com/microservicesstudy/store/resource/StoreResource.java
+++ store/src/main/java/com/microservicesstudy/store/resource/StoreResource.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/stories")
+@RequestMapping(value = "/stores")
 public class StoreResource {
 
     @Autowired
