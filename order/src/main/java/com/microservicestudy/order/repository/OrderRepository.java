d8a7285626822bdad5cbcf68e4cbf57301a7b8b2
daniccarvalho01 <daniela.suppino@gmail.com>
2022-04-14 17:22:33 -0300

    Alterando @QUery de native para JPQL

--- order/src/main/java/com/microservicestudy/order/repository/OrderRepository.java
+++ order/src/main/java/com/microservicestudy/order/repository/OrderRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository <Order, Long> {
 
-    @Query(value = "select * from orders WHERE store_id = ? ORDER BY id DESC LIMIT 2", nativeQuery = true)
+    @Query(value = "select o from orders o WHERE o.store_id = ? ORDER BY id DESC LIMIT 2")
     List<Order> getOrdersByStore(Long storeId);
 
 }
