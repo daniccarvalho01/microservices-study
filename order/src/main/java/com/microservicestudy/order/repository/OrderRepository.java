6940ab1a6f2da15b17ec45da98f775b641d31af7
daniccarvalho01 <daniela.suppino@gmail.com>
2022-04-18 23:07:06 -0300

    Add log.info nos métodos da Resource

--- order/src/main/java/com/microservicestudy/order/repository/OrderRepository.java
+++ order/src/main/java/com/microservicestudy/order/repository/OrderRepository.java
@@ -4,13 +4,14 @@ import com.microservicestudy.order.domain.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository <Order, Long> {
 
-    @Query(value = "select o from orders o WHERE o.store_id = ? ORDER BY id DESC LIMIT 2")
-    List<Order> getOrdersByStore(Long storeId);
+    @Query(value = "select o from orders o WHERE o.store_id = :storeId ORDER BY id DESC LIMIT 2")
+    List<Order> getOrdersByStore(@PathVariable("storeId") Long storeId);
 
 }
