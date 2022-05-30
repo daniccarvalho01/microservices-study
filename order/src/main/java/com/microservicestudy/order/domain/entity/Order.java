7d896877fe587ac2a10990f060246eeea1d1183c
daniccarvalho01 <daniela.suppino@gmail.com>
2022-05-29 23:01:08 -0300

    corrigindo imports

--- order/src/main/java/com/microservicestudy/order/domain/entity/Order.java
+++ order/src/main/java/com/microservicestudy/order/domain/entity/Order.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
